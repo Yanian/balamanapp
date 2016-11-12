e620e993a164e7ee9a566ec48a6181ce30811bb8
clydeinwebdev <clydeinwebdev@gmail.com>
2016-11-13 03:20:44 +0800

    modified server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var cons = require('consolidate');
 var app = express();
 
 app.engine('html',cons.handlebars);
+app.set('port',(process.env.PORT || 3000));
 app.set('view engine', 'html');
 app.set('views', __dirname + '/public');
 app.use(express.static('public'));
@@ -11,6 +12,6 @@ app.get('/', function (req, res) {
   res.render('app',{title: 'Balaman App', message: '<h1>ehem</h1>'});
 });
  
-app.listen(3000,function(){
+app.listen(app.get('port'),function(){
 	console.log("Server is running at localhost:3000");
 });
